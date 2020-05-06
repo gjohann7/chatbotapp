d8c51fd77281dabd5544b26f7a80727d834c25e6
Guilherme Johann <g.johann98@gmail.com>
2020-05-06 14:36:04 +0100

    \\ -> /

--- chatbotapp/settings.py
+++ chatbotapp/settings.py
@@ -2,6 +2,7 @@ import os
 import django_heroku
 from decouple import config, Csv
 import dj_database_url
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
